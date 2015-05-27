c79a405a7d4f34a610e05ad7bb4ac949c3e15372
emilietth <emilietth@yahoo.fr>
2015-05-27 15:28:21 +0200

    Code PHP ajouté

--- formulaire.php
+++ formulaire.php
@@ -6,6 +6,19 @@
     </head>
 
     <body>
+		<?php
+			if (!isset($_POST['mot_de_passe']) OR $_POST['mot_de_passe'] != "kangourou")
+			{
+				if (!isset($_POST['mot_de_passe']))
+				{
+					echo "<p>Veuillez entrer le mot de passe pour obtenir les codes d'accès au serveur central de la NASA :</p>";
+				}		
+				elseif ($_POST['mot_de_passe'] != "kangourou")
+				{
+					echo "<p>Mot de passe incorrect ! Veuillez entrer de nouveau le mot de passe :</p>";				
+				}
+		?>
+		
         <form action="formulaire.php" method="post">
             <p>
             <label for="mot_de_passe">Mot de passe :</label><input type="password" name="mot_de_passe" id="mot_de_passe" />
@@ -13,5 +26,16 @@
             </p>
         </form>
         <p>Cette page est réservée au personnel de la NASA. Si vous ne travaillez pas à la NASA, inutile d'insister vous ne trouverez jamais le mot de passe ! ;-)</p>
+		
+		<?php
+			}
+			else		
+			{
+		?>
+				<h1>Voici le code d'accès : </h1>
+				<p><strong>GDFTYF-UIGU9B6-JI86HTG89</strong></p>
+		<?php
+			}
+		?>
 	</body>
 </html>
